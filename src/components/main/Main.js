15bfbe985d3781a7bb5a7e88b4117ea317f047c5
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-14 11:26:52 +0300

    styles first section of Main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import Button from '../../common/button/Button'
 import MockUps from '../../images/image-mockups.png'
+import './main.css'
 
 const Main = () => {
   return (
     <main>
         <div className="first-section">
             <div className="first-section-left">
-                <h1>Next generation digital banking</h1>
+                <h1>Next generation <span className='digital-banking'>digital banking</span></h1>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit facere aliquid asperiores accusamus cumque. Delectus omnis quibusdam quae aspernatur explicabo sit inventore quis architecto, illum officia ipsam distinctio quisquam aliquam.</p>
                 <Button buttonText={"Request Invite"}/>
             </div>
             <div className="first-section-right">
-                <img src={MockUps} alt="mock ups" />
+                <img src={MockUps} alt="mock ups" className='mock-ups'/>
             </div>
         </div>
         <div className="second-section"></div>
