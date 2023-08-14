8f2be5bfaa189de0586ef683fde9cac81f39bd6b
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-14 10:47:43 +0300

    writes html for fiirst section in main

---
+++ src/components/main/Main.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Button from '../../common/button/Button'
+import MockUps from '../../images/image-mockups.png'
+
+const Main = () => {
+  return (
+    <main>
+        <div className="first-section">
+            <div className="first-section-left">
+                <h1>Next generation digital banking</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit facere aliquid asperiores accusamus cumque. Delectus omnis quibusdam quae aspernatur explicabo sit inventore quis architecto, illum officia ipsam distinctio quisquam aliquam.</p>
+                <Button buttonText={"Request Invite"}/>
+            </div>
+            <div className="first-section-right">
+                <img src={MockUps} alt="mock ups" />
+            </div>
+        </div>
+        <div className="second-section"></div>
+        <div className="third-section"></div>
+    </main>
+  )
+}
+
+export default Main
