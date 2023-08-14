8b1f73f0e9c77f2696fd3c7c1432e76f775f3eb4
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-14 10:26:51 +0300

    designs header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Logo from '../../images/logo.svg'
 import Button from '../../common/button/Button'
+import './header.css'
 
 const Header = () => {
   return (
     <header>
 <img src={Logo} alt="logo" className='logo' />
 <nav>
-<ul>
+<ul className='header-ul'>
     <li><a href="#Home">Home</a></li>
     <li><a href="#About">About</a></li>
     <li><a href="#Contact">Contact</a></li>
